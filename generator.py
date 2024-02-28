fda47de3137842ea1e3804aadbe0e0fe939796dc
leo hjulström <leo.hjulstrom@hotmail.com>
2024-02-28 23:34:36 +0100

    Added way to visualize the generated images easily

--- generator.py
+++ generator.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 from population import Population
 
-
 def main():
+    generated_images = []
     population = Population()
-    for i in range(100):
+    for i in range(2):
         #population.print()
         print(f"-----------------Generation {i+1}-----------------------")
         population.order_by_fitness()
@@ -13,6 +15,9 @@ def main():
         #population.print()
         #print("---------------------------------------")
         population.print()
+        generated_images.append([image.chromosome for image in population.images])
+
+    np.save("generated_images", np.array(generated_images))
 
 if __name__ == '__main__':
     main()
