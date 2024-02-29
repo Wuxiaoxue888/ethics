a6d43d04fcd3765f65da4aa93cc6ce1e0001f799
leo hjulström <leo.hjulstrom@hotmail.com>
2024-02-29 02:08:54 +0100

    Not saving all generated images anymore

--- generator.py
+++ generator.py
@@ -16,9 +16,12 @@ def main():
         #population.print()
         #print("---------------------------------------")
         #population.print()
-        generated_images.append([image.chromosome for image in population.images])
-        if generation % 100 == 0:
-            last_generated_images = np.array(generated_images).reshape(generation*len(population.images),784)[-1000:]
+        if generation % 10 == 0: # save generated images
+            generated_images.append([image.chromosome for image in population.images[:10]])
+
+        if generation % 100 == 0: # train the network more every 100 generations
+            generated_images_1d_array = np.array(generated_images).reshape(np.array(generated_images).shape[0]*np.array(generated_images).shape[1],784)
+            last_generated_images = generated_images_1d_array[-1000:]
             discriminator.train(last_generated_images)
 
     population.print()
