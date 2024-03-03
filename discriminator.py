82d2fe48695a671b61bf84e80e1347861fdf26e8
lukas.toral <lukas.toral@bloomreach.com>
2024-03-03 16:57:29 +0100

    Retrain each discriminator model on different subset of generated images

--- discriminator.py
+++ discriminator.py
@@ -26,7 +26,7 @@ class Discriminator:
 
     def __init__(self):
         self.models = [
-            keras.models.load_model(f"discriminator_models/discriminator_{i+1}.h5")
+            keras.models.load_model(f"discriminator_models/discriminator_{i + 1}.keras")
             for i in range(5)
         ]
 
@@ -38,33 +38,49 @@ class Discriminator:
     def train(self, generated_images):
         generated_images = pd.DataFrame(generated_images)
         generated_images["label"] = 0.0
-        real_images = REAL_IMAGES.sample(frac=1).reset_index(drop=True)[
-            : len(generated_images)
-        ]  # Random subset of real images to train on
-        combined_images = pd.concat(
-            [real_images, generated_images], ignore_index=True
-        )  # Combine the real and fake images
-        combined_images = combined_images.sample(frac=1).reset_index(
-            drop=True
-        )  # Shuffle
+
+        # Random subset of real images to train on
+        real_images = REAL_IMAGES.sample(frac=1).reset_index(drop=True)[: len(generated_images)]
+
+        # Combine the real and fake images
+        combined_images = pd.concat([real_images, generated_images], ignore_index=True)
+        combined_images = combined_images.sample(frac=1).reset_index(drop=True)
+
         features = combined_images.drop("label", axis=1)
         labels = combined_images["label"]
+
         training_size = int(len(combined_images) * 0.8)
+        test_size = int(len(combined_images) * 0.2)
+
+        model_training_size = int(training_size / len(self.models))
+        model_test_size = int(test_size / len(self.models))
+
         X_test, X_train = features[training_size:], features[:training_size]
         y_test, y_train = labels[training_size:], labels[:training_size]
 
-        # Check the current accuract if the model. if its high enough we should not train again
+        # Check the current accuracy of the model. if its high enough we should not train again
         _, accuracy = random.choice(self.models).evaluate(X_test, y_test, verbose=None)
         if accuracy > 0.9:
             print(f"No training, accuracy is {accuracy}")
             return
 
-        for model in self.models:  # Train models again
+        # Train models again
+        for i, model in enumerate(self.models):
+            # create training and test set for the model
+            start_idx = i * model_training_size
+            end_idx = (i + 1) * model_training_size
+
+            m_x_test = features[end_idx:end_idx + model_test_size]
+            m_x_train = features[start_idx:end_idx]
+
+            m_y_test = labels[end_idx:end_idx + model_test_size]
+            m_y_train = labels[start_idx:end_idx]
+
             history = model.fit(
-                X_train,
-                y_train,
+                m_x_train,
+                m_y_train,
                 epochs=1,
-                validation_data=(X_test, y_test),
+                validation_data=(m_x_test, m_y_test),
                 verbose=None,
             )
             print(
