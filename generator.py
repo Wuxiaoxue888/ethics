aac87aa5374676b42d9886a20beff00778f89509
leo hjulström <leo.hjulstrom@hotmail.com>
2024-03-04 13:47:41 +0100

    Created almost real fake images

--- generator.py
+++ generator.py
@@ -1,20 +1,25 @@
 import numpy as np
+import pandas as pd
+import random
 
 from population import Population
 from discriminator import discriminator
 
 # RETRAIN defines after how many generations the discriminator models are retrained.
-RETRAIN_GENERATIONS = 100
+RETRAIN_GENERATIONS = 7
 RETRAIN_FITNESS = 0.85
 
+ALMOST_REAL_IMAGES = pd.read_csv("almost_real_fake_images.csv").drop("label", axis=1).to_numpy().tolist()
 
 def main():
     generated_images = []
-    population = Population()
-    for generation_number in range(1, 200):
+    population = Population(size=512, doping_size=512)
+    for generation_number in range(1, 31):
 
         if generation_number % 10 == 0:
             print(f"-----------------Generation {generation_number}-----------------------")
+
+        population.dope(20)
 
         population.order_by_fitness()
 
@@ -25,17 +30,22 @@ def main():
                 # first two are not BOTH True the third one won't be evaluated and the average_fitness method executed.
                 and population.average_fitness() > RETRAIN_FITNESS
         ):
+            generated_training_images = generated_images[:500]
             discriminator.train(
-                [image.chromosome for image in population.images if not image.doped]
+                generated_training_images + ALMOST_REAL_IMAGES[:len(generated_training_images)]
             )
             generated_images = []
 
         population.create_offsprings(
-            20, 20, selection_function="roulette_wheel", tournament_replacement=False
+            256, 256, selection_function="roulette_wheel"
         )
 
         for image in population.images:
-            generated_images.append(image)
+            if not image.doped:
+                generated_images.append(image.chromosome)
+        generated_images = generated_images[-25_000:]
+        random.shuffle(generated_images)
+        random.shuffle(ALMOST_REAL_IMAGES)
 
     population.print()
 
